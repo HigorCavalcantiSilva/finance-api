052c9b87da03534907f7fe35479db96ccee821f5
Higor Cavalcanti <higor.silva@sesims.com.br>
2025-01-03 13:09:33 -0400

    Add JWT validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const authenticateToken = require('../utils/authenticateToken');
 const UserController = require('../controllers/userController');
 
+// Rotas públicas
 router.get('/login', UserController.login);
+
+// Rotas protegidas
+router.use(authenticateToken);
+
 router.get('/', UserController.getAll);
 router.post('/', UserController.create);
 router.get('/:id', UserController.getById);
