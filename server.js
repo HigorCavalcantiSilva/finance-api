7884d7d8408e3b389d3fcafb4fa39796f44aff12
Higor Cavalcanti <higorsilva07.hs@gmail.com>
2025-01-13 22:17:10 -0400

    Add new routes in main file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const UserRoutes = require('./src/routes/userRoutes');
+const SalaryRoutes = require('./src/routes/salaryRoutes');
+const EarningRoutes = require('./src/routes/earningRoutes');
+const ExpenceRoutes = require('./src/routes/expenceRoutes');
+const SavingRoutes = require('./src/routes/savingRoutes');
 
 const app = express();
 
@@ -10,6 +14,10 @@ app.use(express.json());
 
 // Routes
 app.use('/users', UserRoutes);
+app.use('/salaries', SalaryRoutes);
+app.use('/earnings', EarningRoutes);
+app.use('/expences', ExpenceRoutes);
+app.use('/savings', SavingRoutes);
 
 const port = process.env.PORT || 3000;
 
